de79d857366c1f6d34c245c9cdca595b59493c67
Azad Dilkhosh <azad.21014024@stud.uoz.edu.krd>
2023-12-06 21:34:20 +0300

    form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hw';
+
+  username: string='';
+  email: string='';
+  selectedExperiences: string[] = [];
+  languages: string[] = ['Java', 'Python', 'C++', 'C'];
+  selectedLanguages: string[] = [];
+  description: string='';
+
+  experiences = [
+      'beginner',
+      'Intermediate',
+      'Advanced',
+      'Expert'
+  ];
+
+  onSubmit() {
+      if (this.selectedExperiences.includes('Intermediate'))
+        {
+          alert('Congrats azad your skills are impresive');
+      } else {
+          console.log('Form submitted:', this);
+      }
+  }
 }
